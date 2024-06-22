d3ce8dfbcf53b3ad565a065d25b50d4efb701985
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-22 21:48:46 +0100

    feat: Creation of index.php
    I've added the meta tags and the path to the favicon and styles.

---
+++ source/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Username Getter</title>
+
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+        <meta name="description" content="Username Getter in PHP">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="keywords" content="PHP, HTML, CSS">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="./assets/styles/style.css">
+        <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
+    </head>
+</html>
